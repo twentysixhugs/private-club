c0a2554b5a24fb1623c843d39328bbd10795ec55
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-28 19:37:11 +0300

    Add types for controllers

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,17 @@
+import * as express from 'express';
+
 export interface ResponseError {
   status?: number;
   message?: string;
 }
+
+export type ControllerFn = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => void;
+
+export type AsyncFunctionCallback = (
+  err: Error | null | undefined,
+  result: unknown,
+) => void;
