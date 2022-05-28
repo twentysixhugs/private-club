797ad13c9d836d56c95c5e0963d5ec86c4d73ee3
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-28 20:30:54 +0300

    Make auth form accept options

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,12 @@
 import { ControllerFn } from 'src/types';
 
 const signupGET: ControllerFn = (req, res, next) => {
-  res.render('auth-form');
+  res.render('auth-form', { authAction: 'Sign up', title: 'Sign up' });
 };
 
 const loginGET: ControllerFn = (req, res, next) => {
-  res.render('auth-form');
+  res.render('auth-form', { authAction: 'Log in', title: 'Log in' });
+};
 };
 
 const signupPOST: ControllerFn = (req, res, next) => {};
