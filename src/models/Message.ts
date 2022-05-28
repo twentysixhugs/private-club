fd1b4858996e3758deaae7ace20a27dd9462ad1e
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-28 09:48:10 +0300

    Build models

---
+++ src/models/Message.ts
@@ -0,0 +1,31 @@
+import { Schema, model, Types } from 'mongoose';
+import { DateTime } from 'luxon';
+export interface IMessage {
+  text: String;
+  user: Types.ObjectId;
+  date: Date;
+}
+
+const messageSchema = new Schema<IMessage>({
+  text: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+messageSchema.virtual('dateFormatted').get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(
+    DateTime.DATETIME_MED,
+  );
+});
+
+export default model('Message', messageSchema);
---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import { Schema, model, Types } from 'mongoose';
+
+export interface IUser {
+  username: String;
+  password: String;
+  membership: 'none' | 'member' | 'admin';
+  avatar: '1' | '2' | '3' | '4' | '5';
+}
+
+const userSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+  },
+  membership: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model('User', userSchema);
