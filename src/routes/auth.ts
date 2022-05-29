dfec49e733b76e74148188b32914281bec872870
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-29 13:39:14 +0300

    Add /logout route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,9 +3,12 @@ import * as authController from '../controllers/authController';
 import * as express from 'express';
 const router = express.Router();
 
-router.get('/log-in', authController.loginGET);
-router.post('/log-in', authController.loginPOST);
 router.get('/sign-up', authController.signupGET);
 router.post('/sign-up', authController.signupPOST);
+
+router.get('/log-in', authController.loginGET);
+router.post('/log-in', authController.loginPOST);
+
+router.post('/logout', authController.logoutPOST);
 
 export default router;
